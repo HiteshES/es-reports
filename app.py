6729e5852ef11719193b14c370222f9159e88e19
HiteshES <hitesh.gautam@essentiallysports.com>
2023-04-04 00:17:40 +0530

    Tabs added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from st_aggrid import AgGrid, GridOptionsBuilder, AgGrid, GridUpdateMode, DataRe
 import warnings
 warnings.filterwarnings('ignore')
 
+
 # Visualization 
 import matplotlib as mpl 
 import matplotlib.pyplot as plt 
@@ -25,10 +26,13 @@ from streamlit_echarts import st_echarts
 df = pd.read_csv('./data/content_base.csv')
 
 # Streamlit Formatting 
-st.set_page_config(page_title= "Content Strategy", 
+st.set_page_config(page_title= "Monthly Reports", 
                    page_icon= "https://image-cdn.essentiallysports.com/wp-content/uploads/es_short_logo-1.png", layout= 'wide')
 
-st.markdown("<h1 style='text-align: center; color: #f75453;'>Content Strategy Reports</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: #f75453;'>Monthly Reports</h1>", unsafe_allow_html=True)
+
+
+tab1, tab2 = st.tabs(["CONTENT", "OPS"])
 
 # st.sidebar.header("Filters")
 # editors = st.sidebar.selectbox("Editors:", options= df['editor'].unique())
@@ -130,192 +134,198 @@ total_mar = mar.copy()
 feb = golden_metric(feb)
 mar = golden_metric(mar)
 
+with tab1:
+    st.markdown("<h2 style='text-align: left;'>Current Month - Content Analysis</h1>", unsafe_allow_html=True)
 
-st.markdown("<h2 style='text-align: left;'>Current Month Analysis</h1>", unsafe_allow_html=True)
+    ############################################################ VIZ #############################################
 
-############################################################ VIZ #############################################
+    ####Top 20 Articles
+    cols = ['RANK', 'TITLE', 'SPORTS', 'ENTITY', 'EDITOR', 'WRITER', 'DOMAINS','TOTAL PAGEVIEWS', 
+            'SCROLL RATE', 'PAGE TIME','ENGAGE RATE % (AFTER PARA1)']
 
-####Top 20 Articles
-cols = ['RANK', 'TITLE', 'SPORTS', 'ENTITY', 'EDITOR', 'WRITER', 'DOMAINS','TOTAL PAGEVIEWS', 
-        'SCROLL RATE', 'PAGE TIME','ENGAGE RATE % (AFTER PARA1)']
+    with st.expander("Top 20 Articles", expanded= False):
+        aggrid_interactive_table(mar.loc[:,cols].head(20))
 
-with st.expander("Top 20 Articles", expanded= False):
-    aggrid_interactive_table(mar.loc[:,cols].head(20))
+    # Sportwise
+    sports = mar['SPORTS'].unique().tolist()
+    sportwise = []
+    for sport in sports:
+        sportwise.append(mar[mar['SPORTS'] == sport].sort_values(by = 'GOLDEN', ascending =False).reset_index(drop = True).iloc[:,1:12].head(5))
 
-# Sportwise
-sports = mar['SPORTS'].unique().tolist()
-sportwise = []
-for sport in sports:
-    sportwise.append(mar[mar['SPORTS'] == sport].sort_values(by = 'GOLDEN', ascending =False).reset_index(drop = True).iloc[:,1:12].head(5))
+    sportswise = pd.concat(sportwise)
+    sportswise.reset_index(level=0, inplace=True)
+    sportswise.rename(columns = {'index': 'RANK'}, inplace = True)
 
-sportswise = pd.concat(sportwise)
-sportswise.reset_index(level=0, inplace=True)
-sportswise.rename(columns = {'index': 'RANK'}, inplace = True)
+    with st.expander("Sportwise - Top 5 Articles", expanded=False ):
+        aggrid_interactive_table(sportswise.loc[:,cols])
 
-with st.expander("Sportwise - Top 5 Articles", expanded=False ):
-    aggrid_interactive_table(sportswise.loc[:,cols])
+    # Editorwise
+    editors = mar['EDITOR'].unique().tolist()
+    editor = []
+    for e in editors:
+        editor.append(mar[mar['EDITOR'] == e].sort_values(by = 'GOLDEN',  ascending =False).reset_index(drop = True).iloc[:,1:12].head(5))
 
-# Editorwise
-editors = mar['EDITOR'].unique().tolist()
-editor = []
-for e in editors:
-    editor.append(mar[mar['EDITOR'] == e].sort_values(by = 'GOLDEN',  ascending =False).reset_index(drop = True).iloc[:,1:12].head(5))
+    editorwise = pd.concat(editor)
+    editorwise.reset_index(level=0, inplace=True)
+    editorwise.rename(columns = {'index': 'RANK'}, inplace = True)
 
-editorwise = pd.concat(editor)
-editorwise.reset_index(level=0, inplace=True)
-editorwise.rename(columns = {'index': 'RANK'}, inplace = True)
+    with st.expander("Editorwise - Top 5 Articles", expanded=False ):
+        aggrid_interactive_table(editorwise.loc[:,cols])
 
-with st.expander("Editorwise - Top 5 Articles", expanded=False ):
-    aggrid_interactive_table(editorwise.loc[:,cols])
+    # Entitywise
+    entity = mar['ENTITY'].unique()
+    entities = []
+    for e in entity:
+        entities.append(mar[mar['ENTITY'] == e].sort_values(by = 'GOLDEN',  ascending =False).reset_index(drop = True).iloc[:,1:12].head(5))
 
-# Entitywise
-entity = mar['ENTITY'].unique()
-entities = []
-for e in entity:
-    entities.append(mar[mar['ENTITY'] == e].sort_values(by = 'GOLDEN',  ascending =False).reset_index(drop = True).iloc[:,1:12].head(5))
+    entitywise = pd.concat(entities)
+    entitywise.reset_index(level=0, inplace=True)
+    entitywise.rename(columns = {'index': 'RANK'}, inplace = True)
 
-entitywise = pd.concat(entities)
-entitywise.reset_index(level=0, inplace=True)
-entitywise.rename(columns = {'index': 'RANK'}, inplace = True)
-
-with st.expander("Entitywise - Top 5 Articles", expanded=False ):
-    aggrid_interactive_table(entitywise.loc[:,cols])
-
-
-sources = total_mar.groupby(['domains'], as_index = False)['total pageviews'].sum().sort_values(by = 'total pageviews', ascending = False).reset_index(drop = True).head(5)
+    with st.expander("Entitywise - Top 5 Articles", expanded=False ):
+        aggrid_interactive_table(entitywise.loc[:,cols])
 
 
-### Source Chart
-# Visualization variables initialization
-
-background = "#404040"
-text_color = "w"
-
-mpl.rcParams['xtick.color'] = text_color
-mpl.rcParams['ytick.color'] = text_color
-mpl.rcParams['xtick.labelsize'] = 10
-mpl.rcParams['ytick.labelsize'] = 10
-
-filler = "grey"
-primary ="red"
-
-title_font = "Kanit"
-
-# creating figure and axes
-fig, ax = plt.subplots(figsize=(16,7))
-fig.set_facecolor(background)
-ax.patch.set_facecolor(background)
-
-# adding a grid with zorder and style
-ax.grid(ls="dotted",lw="0.3",color="lightgrey", zorder= 1, visible = True)
-
-# Create data for pie chart
-sizes = sources['total pageviews']
-labels = sources['domains']
-colors = ['#F94144', '#F8961E', '#F9C74F', '#90BE6D', '#43AA8B']
-
-# set the width and height of the chart
-width = 0.2
-height = 0.3
-
-# set the position of the chart within the axis
-left = 0.1
-bottom = 0.1
-
-# Create pie chart with logo
-text_props = {'color': 'white', 'fontsize': 12}
-ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90, radius=0.8, counterclock=False,
-       textprops=text_props)
-
-# Add legend
-ax.legend(labels, title="Sources", loc="lower right", bbox_to_anchor=(1, 0, 0.5, 1))
-
-# For badge: Adding new axes
-ax1 = fig.add_axes([0.04,0.99,0.1,0.14])
-ax1.axis("off")
-
-url = "https://image-cdn.essentiallysports.com/wp-content/uploads/es_short_logo-1.png"
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-ax1.imshow(img)     
-
-# Adding Credits
-fig.text(0.05, -0.028, "Data powered by content strategy", fontstyle="italic", fontsize=12, 
-         color=text_color)
-
-plt.tight_layout()
-
-st.markdown("<h4> Top 5 sources</h4>", unsafe_allow_html=True)
-
-st.pyplot(plt.gcf())
+    sources = total_mar.groupby(['domains'], as_index = False)['total pageviews'].sum().sort_values(by = 'total pageviews', ascending = False).reset_index(drop = True).head(5)
 
 
+    ### Source Chart
+    # Visualization variables initialization
 
-# MoM comparison 
-feb_count = feb.groupby(['SPORTS'], as_index = False)['TITLE'].nunique()
-mar_count = mar.groupby(['SPORTS'], as_index = False)['TITLE'].nunique()
+    background = "#404040"
+    text_color = "w"
 
-st.markdown("<h4> MoM comparison of #articles having > 10K </h4>", unsafe_allow_html=True)
-# Create data for pie chart
-labels = feb_count.SPORTS
-feb_count = feb_count.TITLE
-mar_count = mar_count.TITLE
+    mpl.rcParams['xtick.color'] = text_color
+    mpl.rcParams['ytick.color'] = text_color
+    mpl.rcParams['xtick.labelsize'] = 10
+    mpl.rcParams['ytick.labelsize'] = 10
 
-# Define the options for the chart
-options = {
-    "legend": {
-                "data": ['Feb', 'Mar'],   
-                "textStyle": {
+    filler = "grey"
+    primary ="red"
+
+    title_font = "Kanit"
+
+    # creating figure and axes
+    fig, ax = plt.subplots(figsize=(16,7))
+    fig.set_facecolor(background)
+    ax.patch.set_facecolor(background)
+
+    # adding a grid with zorder and style
+    ax.grid(ls="dotted",lw="0.3",color="lightgrey", zorder= 1, visible = True)
+
+    # Create data for pie chart
+    sizes = sources['total pageviews']
+    labels = sources['domains']
+    colors = ['#F94144', '#F8961E', '#F9C74F', '#90BE6D', '#43AA8B']
+
+    # set the width and height of the chart
+    width = 0.2
+    height = 0.3
+
+    # set the position of the chart within the axis
+    left = 0.1
+    bottom = 0.1
+
+    # Create pie chart with logo
+    text_props = {'color': 'white', 'fontsize': 12}
+    ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90, radius=0.8, counterclock=False,
+        textprops=text_props)
+
+    # Add legend
+    ax.legend(labels, title="Sources", loc="lower right", bbox_to_anchor=(1, 0, 0.5, 1))
+
+    # For badge: Adding new axes
+    ax1 = fig.add_axes([0.04,0.99,0.1,0.14])
+    ax1.axis("off")
+
+    url = "https://image-cdn.essentiallysports.com/wp-content/uploads/es_short_logo-1.png"
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    ax1.imshow(img)     
+
+    # Adding Credits
+    fig.text(0.05, -0.028, "Data powered by content strategy", fontstyle="italic", fontsize=12, 
+            color=text_color)
+
+    plt.tight_layout()
+
+    st.markdown("<h4> Top 5 sources</h4>", unsafe_allow_html=True)
+
+    st.pyplot(plt.gcf())
+
+
+
+    # MoM comparison 
+    feb_count = feb.groupby(['SPORTS'], as_index = False)['TITLE'].nunique()
+    mar_count = mar.groupby(['SPORTS'], as_index = False)['TITLE'].nunique()
+
+    st.markdown("<h4> MoM comparison of #articles having > 10K </h4>", unsafe_allow_html=True)
+    # Create data for pie chart
+    labels = feb_count.SPORTS
+    feb_count = feb_count.TITLE
+    mar_count = mar_count.TITLE
+
+    # Define the options for the chart
+    options = {
+        "legend": {
+                    "data": ['Feb', 'Mar'],   
+                    "textStyle": {
+                            'color': 'white', # Set the color of the value labels to white
+                            'fontWeight': 'bold', # Set the font weight of the value labels to bold
+                            'fontSize': 12 # Set the font size of the value labels to 14 pixels
+                    }
+            },
+        "xAxis": {"data": labels.tolist(),    
+                'axisLabel': {
+                    'textStyle': {
+                        'color': 'white', # Set the color of the label text to red
+                        'fontSize': 12 # Set the font size of the label text to 14 pixels
+                    }
+                }
+            },
+        "yAxis": {},
+        "series": [
+            {   "name": "Feb", 
+                "type": "bar", 
+                "data":feb_count.tolist(),  
+                "label": {
+                    "show": True,
+                    "position": "top",
+                    'textStyle': {
                         'color': 'white', # Set the color of the value labels to white
                         'fontWeight': 'bold', # Set the font weight of the value labels to bold
-                        'fontSize': 12 # Set the font size of the value labels to 14 pixels
-                }
-        },
-    "xAxis": {"data": labels.tolist(),    
-              'axisLabel': {
-                'textStyle': {
-                    'color': 'white', # Set the color of the label text to red
-                    'fontSize': 12 # Set the font size of the label text to 14 pixels
-                }
-            }
-        },
-    "yAxis": {},
-    "series": [
-        {   "name": "Feb", 
-            "type": "bar", 
-            "data":feb_count.tolist(),  
-            "label": {
-                "show": True,
-                "position": "top",
-                 'textStyle': {
-                    'color': 'white', # Set the color of the value labels to white
-                    'fontWeight': 'bold', # Set the font weight of the value labels to bold
-                    'fontSize': 14 # Set the font size of the value labels to 14 pixels
+                        'fontSize': 14 # Set the font size of the value labels to 14 pixels
+                    }
+                },
+                "itemStyle": {
+                    "color": "#F94144"
                 }
             },
-            "itemStyle": {
-                "color": "#F94144"
-            }
-        },
-        {   "name": "Mar", 
-            "type": "bar", 
-            "data": mar_count.tolist(),  
-            "label": {
-                "show": True,
-                "position": "top",
-                'textStyle': {
-                    'color': 'white', # Set the color of the value labels to white
-                    'fontWeight': 'bold', # Set the font weight of the value labels to bold
-                    'fontSize': 14 # Set the font size of the value labels to 14 pixels
+            {   "name": "Mar", 
+                "type": "bar", 
+                "data": mar_count.tolist(),  
+                "label": {
+                    "show": True,
+                    "position": "top",
+                    'textStyle': {
+                        'color': 'white', # Set the color of the value labels to white
+                        'fontWeight': 'bold', # Set the font weight of the value labels to bold
+                        'fontSize': 14 # Set the font size of the value labels to 14 pixels
+                    }
+                },
+                "itemStyle": {
+                    "color": "#F8961E"
                 }
             },
-            "itemStyle": {
-                "color": "#F8961E"
-            }
-        },
-    ],
+        ],
 
-}
+    }
 
-# Display the chart using the st_echarts function
-st_echarts(options=options, height="400px")
+    # Display the chart using the st_echarts function
+    st_echarts(options=options, height="400px")
+
+
+with tab2:
+    st.markdown("<h2 style='text-align: left;'>Current Month - Ops Analysis</h1>", unsafe_allow_html=True)
+
+    
